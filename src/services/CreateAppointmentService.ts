71de248eb08091504481be461315ec8ed30c28a4
Joel Fragoso <joelfragoso85@icloud.com>
2020-06-23 09:04:47 -0300

    feature: Added services and SOLID

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: Request): Appointment {
+    const appointmentHour = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentHour,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentHour,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
