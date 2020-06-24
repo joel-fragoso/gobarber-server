d0f67716b1f7e73b911f98efa9630e988f30a30a
Joel Fragoso <joelfragoso85@icloud.com>
2020-06-24 08:42:35 -0300

    feature: Added relationship models

--- src/database/migrations/1592942128165-CreateUsers.ts
+++ src/database/migrations/1592942128165-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1592942128165 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
