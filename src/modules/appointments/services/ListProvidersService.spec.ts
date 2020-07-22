930fdbf407aae4777caad621e39c3ddd2f9dbb4d
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-22 15:21:32 -0300

    feat: add providers list

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,41 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProvidersService: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProvidersService = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'secret',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'Marie Doe',
+      email: 'mariedoe@example.com',
+      password: 'secret',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Joel Fragoso',
+      email: 'joelfragoso85@icloud.com',
+      password: 'secret',
+    });
+
+    const providers = await listProvidersService.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
