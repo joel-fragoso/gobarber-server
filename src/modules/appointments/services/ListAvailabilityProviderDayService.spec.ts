3fb3a691b67de9cb2570014be71aeec7d93cbce7
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-23 10:06:18 -0300

    feat: deleting old times

--- src/modules/appointments/services/ListAvailabilityProviderDayService.spec.ts
+++ src/modules/appointments/services/ListAvailabilityProviderDayService.spec.ts
@@ -18,12 +18,16 @@ describe('ListAvailabilityProviderDay', () => {
   it('should be able to list the availability of the provider in day', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: '123',
-      date: new Date(2020, 6, 22, 8, 0, 0),
+      date: new Date(2020, 6, 22, 13, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: '123',
-      date: new Date(2020, 6, 22, 10, 0, 0),
+      date: new Date(2020, 6, 22, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 6, 22, 11, 0, 0).getTime();
     });
 
     const availability = await listAvailabilityProviderDayService.execute({
@@ -36,9 +40,15 @@ describe('ListAvailabilityProviderDay', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 11, available: false },
+        { hour: 12, available: true },
+        { hour: 13, available: false },
+        { hour: 14, available: true },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+        { hour: 17, available: true },
       ]),
     );
   });
