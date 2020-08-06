95a1c068584c066b461aeec0972c9abb79f2ab07
Joel Fragoso <joelfragoso85@icloud.com>
2020-08-06 08:51:00 -0300

    fix: list providers class-transformer

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
+import { classToClass } from 'class-transformer';
+
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
 class ProvidersController {
@@ -13,7 +15,7 @@ class ProvidersController {
       user_id,
     });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
 
